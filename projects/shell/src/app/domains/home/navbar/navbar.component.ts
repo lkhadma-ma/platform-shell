4ea6670d84b842cf3345445e3729b4901510fffd
OUSSAMA YAAGOUB <oyaagoub@externas.sescam.jccm.es>
2025-09-18 18:17:04 +0200

    update navbar.component.html and navbar.component.ts

--- projects/shell/src/app/domains/home/navbar/navbar.component.ts
+++ projects/shell/src/app/domains/home/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { LinkItemComponent } from './components/link-item/link-item.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  imports: [LinkItemComponent]
+  imports: [LinkItemComponent, RouterLink]
 })
 export class NavbarComponent implements OnInit {
 
