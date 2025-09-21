65b7aa258d414fe065c8607dca92b6d16b8b0d5a
oussamayaagoub <oussamayaagoub@users.noreply.github.com>
2025-09-21 21:41:26 +0000

    deploy: b32c0335692c5fa6eecf35db5b91bcf33f541536

---
+++ main-BAS5HVFV.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-BJE3JKBP.js";import"./chunk-2VMXMS7J.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-XFLIHUIU.js")).catch(o=>console.error(o));
