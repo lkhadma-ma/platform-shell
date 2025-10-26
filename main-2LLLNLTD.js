4798c676b09acccc22d72fce19035cfdd00be959
oussamayaagoub <oussamayaagoub@users.noreply.github.com>
2025-10-26 18:01:58 +0000

    deploy: 06b11986a8aab2ef0dc92fa1248c71b44a218dea

---
+++ main-2LLLNLTD.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-BJE3JKBP.js";import"./chunk-2VMXMS7J.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-MIVG3HCA.js")).catch(o=>console.error(o));
