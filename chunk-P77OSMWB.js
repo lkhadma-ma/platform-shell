8dbe5511244b7b1daeac739f81719f2f0016ef2d
oussamayaagoub <oussamayaagoub@users.noreply.github.com>
2025-10-30 21:03:52 +0000

    deploy: 8f6be4ed2dac8d3159fd40a165db52146ca4570b

--- chunk-P77OSMWB.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-54YWSZMQ.js").then(o=>o.ProfileComponent)}];export{t as PROFILE_ROUTES};
---
+++ chunk-VGME4F7Z.js
@@ -0,0 +1,1 @@
+import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-JAOYGUYP.js").then(o=>o.ProfileComponent)}];export{t as PROFILE_ROUTES};
