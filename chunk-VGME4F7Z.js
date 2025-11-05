33ecb8fd43eea33414b7568c582c18be8a3d0a88
oussamayaagoub <oussamayaagoub@users.noreply.github.com>
2025-11-05 23:36:51 +0000

    deploy: 91391b02ebc395cd159a637f23ec0ca0ad089dc8

--- chunk-VGME4F7Z.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-JAOYGUYP.js").then(o=>o.ProfileComponent)}];export{t as PROFILE_ROUTES};
--- main-V5SCPJGE.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./chunk-BJE3JKBP.js";import"./chunk-2VMXMS7J.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-SD4B6SLW.js")).catch(o=>console.error(o));
