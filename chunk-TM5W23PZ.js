1fdac68eb0f8c0f0b921a783fe9b911fcb0c5930
oussamayaagoub <oussamayaagoub@users.noreply.github.com>
2025-11-24 21:19:08 +0000

    deploy: a3cd1f2213ed832196ee88248390ed985c7a6b18

---
+++ chunk-TM5W23PZ.js
@@ -0,0 +1,1 @@
+import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-YFT5HQUP.js").then(o=>o.ProfileComponent)}];export{t as PROFILE_ROUTES};
--- chunk-W2HCSVAC.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-RJZNWKLD.js").then(o=>o.ProfileComponent)}];export{t as PROFILE_ROUTES};
